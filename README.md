f9efe59681b02a009bf58e7a67f2b3c7ca2b6959
Steven Raden <stevengraden@gmail.com>
2017-02-28 22:43:09 -0600

    Finished tutorial with some small changes

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# vue-chart-tutorial
-Followed Tutorial from Medium article on how to quickly setup a chart for VueJS
+# vue-charts
+
+> A tutorial for using vue and charts
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="Chart__list">
+        <div class="Chart">
+          <h2>Linechart 1</h2>
+          <line-chart></line-chart>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="Chart__list">
+        <div class="Chart">
+          <h2>Linechart 2</h2>
+          <line-chart></line-chart>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="Chart__list">
+        <div class="Chart">
+          <h2>Linechart 3</h2>
+          <line-chart></line-chart>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="Chart__list">
+        <div class="Chart">
+          <h2>Linechart 4</h2>
+          <line-chart></line-chart>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LineChart from './components/LineChart.js'
+export default {
+  name: 'app',
+  components: {
+    LineChart
+  }
+}
+</script>
